b0b9ca6420b2488eafd534ca68a3649e58c84f9b
Marcin Cieśliński <marcinc783@gmail.com>
2023-04-30 14:53:03 +0200

    Wersja po przejściach

--- app/src/main/java/com/wakacjeapp/client_interface/MainClientMenu.kt
+++ app/src/main/java/com/wakacjeapp/client_interface/MainClientMenu.kt
@@ -1,5 +1,6 @@
 package com.wakacjeapp.client_interface
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -17,12 +18,12 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.wakacjeapp.Messages.NewMessageActivity
 import com.wakacjeapp.R
+import com.wakacjeapp.User
 import com.wakacjeapp.adapter.MainMenuAdapter
 import com.wakacjeapp.databinding.ActivityMainClientMenuBinding
 import com.wakacjeapp.model.*
 import com.wakacjeapp.trip.model.Trip
 import com.wakacjeapp.trip.model.TripDay
-import java.time.LocalDate
 import java.util.Calendar
 
 
@@ -59,12 +60,265 @@ class MainClientMenu : AppCompatActivity() {
 
         main_binding.menuAllButton.setOnClickListener {
             val intent = Intent(this, NewMessageActivity::class.java)
-            startActivity(intent)
+
+            dodajUzytkownikaDoWycieczki("-NUGxERHNwRHJLWM7p86", User(name = "Marcin", email = "marcin@wp.pl", uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"))
+            //startActivity(intent)
         }
 
         main_binding.ShowAccountButton.setOnClickListener {
+            dodajNoweWycieczki()
             Toast.makeText(this@MainClientMenu, "Konto użytkownika", Toast.LENGTH_SHORT).show()
         }
+
+
+    }
+
+    fun dodajUzytkownikaDoWycieczki(wycieczkaId: String, nowyUzytkownik: User) {
+        val database = FirebaseDatabase.getInstance()
+        val wycieczkiRef = database.getReference("wycieczki")
+
+        val wycieczkaRef = wycieczkiRef.child(wycieczkaId)
+
+        wycieczkaRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val wycieczka = dataSnapshot.getValue(Trip::class.java) // Pobiera aktualne wartości wycieczki
+                wycieczka?.uzytkownicy?.add(nowyUzytkownik) // Dodaje uzytkownika
+                wycieczkaRef.setValue(wycieczka) // Aktualizuje wartości
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    fun dodajNowaWycieczka(nowaWycieczka: Trip) {
+        val database = FirebaseDatabase.getInstance()
+        val wycieczkiRef = database.getReference("wycieczki")
+
+        val nowaWycieczkaRef = wycieczkiRef.push()
+        val nowaWycieczkaKey = nowaWycieczkaRef.key
+
+        val nowaWycieczkaMap = hashMapOf(
+            "cena_za_osobe" to nowaWycieczka.cena,
+            "data_wyjazdu" to nowaWycieczka.data_pocz,
+            "data_powrotu" to nowaWycieczka.data_powrotu,
+            "miejsce" to nowaWycieczka.miejsce,
+            "kraj" to nowaWycieczka.kraj,
+            "ocena" to nowaWycieczka.ocena,
+            "opis" to nowaWycieczka.opis,
+            "zdjecie" to nowaWycieczka.zdjecie,
+            "ilosc_miejsc" to nowaWycieczka.ilosc_miejsc,
+            "id_wycieczki" to nowaWycieczkaKey,
+            "plan_wycieczki" to nowaWycieczka.plan,
+            "uzytkownicy" to nowaWycieczka.uzytkownicy
+        )
+
+        nowaWycieczkaRef.setValue(nowaWycieczkaMap)
+    }
+
+    fun dodajNoweWycieczki(){
+
+        val daysList: ArrayList<TripDay> = ArrayList()
+        daysList.add(TripDay(1, "Przylot do Rzymu o godzinie 14:00. Zakwaterowanie w hotelu i czas wolny na zwiedzanie miasta."))
+        daysList.add(TripDay(2, "Zwiedzanie Rzymu: Koloseum, Forum Romanum, Pałac Cezarów, Fontanna di Trevi, Panteon." ))
+        daysList.add(TripDay(3,  "Wyjazd na wycieczkę poza miasto: Watykan, Muzea Watykańskie, Bazylika św. Piotra."))
+        daysList.add(TripDay(4,  "Czas wolny na zakupy i odpoczynek. Wyjazd powrotny do kraju o godzinie 19:00."))
+
+        val uzytkownicy: ArrayList<User> = ArrayList()
+        uzytkownicy.add( User(name = "Marcin", email = "marcin@wp.pl", uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"))
+
+        val trip = Trip(
+            cena = 1000.0,
+            data_pocz = "2023-06-03",
+            data_powrotu = "2023-06-01",
+            miejsce = "Rzym",
+            kraj = "Włochy",
+            ocena = 4.8,
+            opis = "Zwiedzanie Rzymu i okolic",
+            zdjecie = "wlochy.png",
+            ilosc_miejsc = 16,
+            plan = daysList,
+            uzytkownicy = uzytkownicy,
+
+        )
+
+        dodajNowaWycieczka(trip)
+
+//        val trip1 = Trip(
+//            cena = 800.0,
+//            data_pocz = "2023-07-10",
+//            data_powrotu = "2023-07-03",
+//            miejsce = "Barcelona",
+//            kraj = "Hiszpania",
+//            ocena = 4.5,
+//            opis = "Zwiedzanie Barcelony i Costa Brava",
+//            zdjecie = "hiszpania.png",
+//            ilosc_miejsc = 10,
+//            plan = listOf(
+//                TripDay(
+//                    numer = 1,
+//                    opis = "Przylot do Barcelony o godzinie 12:00. Zakwaterowanie w apartamencie i czas wolny na zwiedzanie miasta."
+//                ),
+//                TripDay(
+//                    numer = 2,
+//                    opis = "Zwiedzanie Barcelony: La Rambla, Park Güell, Sagrada Familia, Katedra św. Eulalii."
+//                ),
+//                TripDay(
+//                    numer = 3,
+//                    opis = "Wyjazd na wycieczkę na Costa Brava: Tossa de Mar, Lloret de Mar, Blanes."
+//                ),
+//                TripDay(
+//                    numer = 4,
+//                    opis = "Czas wolny na zakupy i odpoczynek. Wyjazd powrotny do kraju o godzinie 18:00."
+//                )
+//            ) as ArrayList<TripDay>,
+//            uzytkownicy = listOf(
+//                User(
+//                    name = "Marcin",
+//                    email = "marcin@wp.pl",
+//                    uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"
+//                ),
+//                User(
+//                    name = "Maksymilian",
+//                    email = "maksymilian@wp.pl",
+//                    uid = "r7OxkN0jERgqw4OBQ5qQwvytjuf2"
+//                ),
+//            )  as ArrayList<User>
+//        )
+//
+//        val trip2 = Trip(
+//            cena = 1200.0,
+//            data_pocz = "2023-09-05",
+//            data_powrotu = "2023-06-01",
+//            miejsce = "Ateny",
+//            kraj = "Grecja",
+//            ocena = 4.9,
+//            opis = "Zwiedzanie Aten i wyspy Santorini",
+//            zdjecie = "grecja.png",
+//            ilosc_miejsc = 16,
+//            plan = listOf(
+//                TripDay(
+//                    numer = 1,
+//                    opis = "Przylot do Aten o godzinie 11:00. Zakwaterowanie w hotelu i czas wolny na zwiedzanie miasta."
+//                ),
+//                TripDay(
+//                    numer = 2,
+//                    opis ="Zwiedzanie Aten: Akropol, Agora Rzymska, Muzeum Akropolu, Plaka."
+//                ),
+//                TripDay(
+//                    numer = 3,
+//                    opis = "Wyjazd na wycieczkę na wyspę Santorini: Oia, Fira, Kamari, Czerwone Plaże."
+//                ),
+//                TripDay(
+//                    numer = 4,
+//                    opis = "Czas wolny na zakupy i odpoczynek. Wyjazd powrotny do kraju o godzinie 20:00."
+//                )
+//            ) as ArrayList<TripDay>,
+//            uzytkownicy = listOf(
+//                User(
+//                    name = "Marcin",
+//                    email = "marcin@wp.pl",
+//                    uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"
+//                ),
+//                User(
+//                    name = "Agata P",
+//                    email = "agata@wp.pl",
+//                    uid = "6mb3E3G0XDZMBFoRjcLc9Jdyq4v2"
+//                ),
+//            ) as ArrayList<User>
+//        )
+//
+//        val trip3 = Trip(
+//            cena = 2000.0,
+//            data_pocz = "2023-12-10",
+//            data_powrotu = "2023-12-01",
+//            miejsce = "Chartum",
+//            kraj = "Sudan",
+//            ocena = 4.2,
+//            opis = "Safari w Parku Narodowym Dżabal Kurdufan",
+//            zdjecie = "Sudan.png",
+//            ilosc_miejsc = 10,
+//            plan = listOf(
+//                TripDay(
+//                    numer = 1,
+//                    opis = "Przylot do Chartumu o godzinie 8:00. Zakwaterowanie w hotelu i czas wolny na zwiedzanie miasta."
+//                ),
+//                TripDay(
+//                    numer = 2,
+//                    opis = "Wyjazd na safari w Parku Narodowym Dżabal Kurdufan: zwiedzanie pięknych krajobrazów, spotkanie z dzikimi zwierzętami."
+//                ),
+//                TripDay(
+//                    numer = 3,
+//                    opis = "Zwiedzanie historycznych miejsc Chartumu: Muzeum Narodowe, Naga, Gebel Barkal."
+//                ),
+//                TripDay(
+//                    numer = 4,
+//                    opis = "Czas wolny na zakupy i odpoczynek. Wyjazd powrotny do kraju o godzinie 19:00."
+//                )
+//            ) as ArrayList<TripDay>,
+//            uzytkownicy = listOf(
+//                User(
+//                    name = "Marcin",
+//                    email = "marcin@wp.pl",
+//                    uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"
+//                ),
+//                User(
+//                    name = "Aleksandra",
+//                    email = "aleksandra@wp.pl",
+//                    uid = "0nnht96CZhSABjTYlWzT6Beh7lw1"
+//                ),
+//            )  as ArrayList<User>
+//        )
+//
+//        val trip4 = Trip(
+//            cena = 3000.0,
+//            data_pocz = "2023-10-30",
+//            data_powrotu = "2023-10-10",
+//            miejsce = "Isfahan",
+//            kraj = "Iran",
+//            ocena = 4.8,
+//            opis = "Zwiedzanie zabytków i kulinarnych przysmaków Iranu",
+//            zdjecie = "hiszpania.png",
+//            ilosc_miejsc = 8,
+//            plan = listOf(
+//                TripDay(
+//                    numer = 1,
+//                    opis = "Przylot do Isfahan o godzinie 11:00. Zakwaterowanie w hotelu i czas wolny na zwiedzanie miasta."
+//                ),
+//                TripDay(
+//                    numer = 2,
+//                    opis = "Zwiedzanie zabytków Isfahanu: Most Si-o-seh Pol, Pałac Haszty Beheszt, Meczet Szejka Lotfollaha."
+//                ),
+//                TripDay(
+//                    numer = 3,
+//                    opis = "Wycieczka do Perspolis: zwiedzanie ruiny starożytnego miasta i Muzeum Archeologicznego w Sziraz."
+//                ),
+//                TripDay(
+//                    numer = 4,
+//                    opis = "Degustacja kulinarnych przysmaków Iranu i czas wolny na zakupy. Wyjazd powrotny do kraju o godzinie 20"
+//                )
+//            ) as ArrayList<TripDay>,
+//            uzytkownicy = listOf(
+//                User(
+//                    name = "Marcin",
+//                    email = "marcin@wp.pl",
+//                    uid = "f8OGF4FoJkMkOY69ykZ4l4K0geK2"
+//                ),
+//                User(
+//                    name = "Maksymilian",
+//                    email = "maksymilian@wp.pl",
+//                    uid = "r7OxkN0jERgqw4OBQ5qQwvytjuf2"
+//                ),
+//                User(
+//                    name = "Aleksandra",
+//                    email = "aleksandra@wp.pl",
+//                    uid = "0nnht96CZhSABjTYlWzT6Beh7lw1"
+//                ),
+//            )  as ArrayList<User>
+//        )
+
 
 
     }
@@ -96,8 +350,6 @@ class MainClientMenu : AppCompatActivity() {
                 for (ds in dataSnapshot.children) {
                     val offer = ds.child("offer").getValue(String::class.java)
                     val image = ds.child("image").getValue(Int::class.java)!!
-                    val trip = Trip(currentDate,20.0,currentDate,"Pustynia","Iran",2.3,"Szybki opis",tripDays)
-                    tripsList.add(trip)
 
                 }
 
@@ -126,6 +378,18 @@ class MainClientMenu : AppCompatActivity() {
         }
         tripsRef.addValueEventListener(valueEventListener)
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //    private fun getTripsList() {
--- app/src/main/java/com/wakacjeapp/trip/model/Trip.kt
+++ app/src/main/java/com/wakacjeapp/trip/model/Trip.kt
@@ -1,26 +1,34 @@
 package com.wakacjeapp.trip.model
 
-import java.util.Date
+import com.wakacjeapp.User
 
 class Trip {
     var cena: Double
-    var data_pocz: Date
-    var data_powrotu: Date
+    var data_pocz: String
+    var data_powrotu: String
     var miejsce: String
     var kraj: String
     var ocena: Double
     var opis: String
-    var plan: ArrayList<TripDay>
+    var zdjecie: String
+    var ilosc_miejsc: Int
+    val plan: ArrayList<TripDay>
+    var uzytkownicy: ArrayList<User>
+    private lateinit var id_wycieczki: String
 
     constructor(
-        data_pocz: Date,
+        data_pocz: String,
         cena: Double,
-        data_powrotu: Date,
+        data_powrotu: String,
         miejsce: String,
         kraj: String,
         ocena: Double,
         opis: String,
-        plan: ArrayList<TripDay>
+        zdjecie: String,
+        ilosc_miejsc: Int,
+        plan: ArrayList<TripDay>,
+        uzytkownicy: ArrayList<User>,
+        id_wycieczki: String
     ) {
 
         this.data_pocz = data_pocz
@@ -30,7 +38,38 @@ class Trip {
         this.kraj = kraj
         this.ocena = ocena
         this.opis = opis
+        this.ilosc_miejsc = ilosc_miejsc
         this.plan = plan
+        this.zdjecie = zdjecie
+        this.uzytkownicy = uzytkownicy
+        this.id_wycieczki = id_wycieczki
+    }
+    constructor(
+        data_pocz: String,
+        cena: Double,
+        data_powrotu: String,
+        miejsce: String,
+        kraj: String,
+        ocena: Double,
+        opis: String,
+        zdjecie: String,
+        ilosc_miejsc: Int,
+        plan: ArrayList<TripDay>,
+        uzytkownicy: ArrayList<User>
+    ) {
+
+        this.data_pocz = data_pocz
+        this.cena = cena
+        this.data_powrotu = data_powrotu
+        this.miejsce = miejsce
+        this.kraj = kraj
+        this.ocena = ocena
+        this.opis = opis
+        this.ilosc_miejsc = ilosc_miejsc
+        this.plan = plan
+        this.zdjecie = zdjecie
+        this.uzytkownicy = uzytkownicy
+
     }
 
 
